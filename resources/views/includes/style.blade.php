baa9257e3262a205a0b7ee473f4e611d0891d10b
ahya37 <ahmadyaniid37@gmail.com>
2021-06-22 16:43:02 +0700

    edit tag

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,5 +1,5 @@
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-    <link href="/style/main.css" rel="stylesheet" />
+    <link href="{{ asset('style/main.css') }}" rel="stylesheet" />
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
