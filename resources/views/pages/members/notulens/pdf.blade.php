83a7f3663aa3ccadb5fc812c78deb11371264e22
ahya37 <ahmadyaniid37@gmail.com>
2021-06-23 11:01:00 +0700

    ttd

--- resources/views/pages/members/notulens/pdf.blade.php
+++ resources/views/pages/members/notulens/pdf.blade.php
@@ -23,7 +23,7 @@
 
             footer {
                 position: fixed; 
-                bottom: -40px; 
+                bottom: -10px; 
                 left: 0px; 
                 right: 0px;
                 height: 100px; 
